beab907ffbd305dbdd89e3ba7d1912607f56a5ce
hiu9coceate <hiubao237@gmail.com>
2025-05-26 11:34:12 +0700

    add b3

--- lab-01/ex02/ex02_08.py
+++ lab-01/ex02/ex02_08.py
@@ -4,7 +4,7 @@ def chia_het_cho_5(so_nhi_phan):
         return True
     else:
         return False
-chuoi_so_nhi_phan = input("Nhap chuoi so nhi phan(phan tach boi dau ,): ")
+chuoi_so_nhi_phan = input("Nhap chuoi so nhi phan(phan tach boi dau ,)VD:1010,1100..: ")
 so_nhi_phan_list = chuoi_so_nhi_phan.split(',')
 so_chia_het_cho_5 = [so for so in so_nhi_phan_list if chia_het_cho_5(so)]
 if len(so_chia_het_cho_5) > 0:
